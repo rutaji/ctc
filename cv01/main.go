651cfe7f85c10dbd53483c08988cd30243ff8176
rutaji <jiri.ruta@tul.cz>
2024-04-21 21:59:50 +0200

    pumpstation

--- cv01/main.go
+++ cv01/main.go
@@ -1,28 +1,37 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 type car struct {
-	id   int
-	fuel int
+	id         int
+	fuel       int
+	isTankFull bool
 }
 type pump struct {
 	fuel          int
-	line          *list.List
+	line          []*car
 	maxCarsInLine int
+	minTime       int
+	maxTime       int
 }
 type carFactory struct {
 	carId int
 }
+type gasStation struct {
+	pumps []*pump
+
+}
+type 
 
 func (carFactory *carFactory) getCar() car {
 	var car car
 	car.id = carFactory.getId()
 	car.fuel = rand.Intn(numberOfFuel)
+	car.isTankFull = false
 	return car
 }
 func (carFactory *carFactory) getId() int {
@@ -30,7 +39,34 @@ func (carFactory *carFactory) getId() int {
 	return carFactory.carId
 }
 func (pump *pump) canGetInLine(car *car) bool {
-	return pump.fuel == car.fuel && pump.line.Len() <= pump.maxCarsInLine
+	return pump.fuel == car.fuel && len(pump.line) <= pump.maxCarsInLine
+}
+func (pump *pump) getCarsInLine() int {
+	return len(pump.line)
+}
+func (pump *pump) serve() {
+	var randTime = rand.Intn(pump.maxTime-pump.minTime) + pump.minTime
+	time.Sleep(time.Duration(randTime) * time.Millisecond)
+}
+func (pump *pump) mainLoop() {
+	for {
+		if !pump.line[0].isTankFull {
+			pump.serve()
+			pump.line[0].isTankFull = true
+			fmt.Printf("car %d is full", pump.line[0].id)
+		}
+		//waiting for payment
+	}
+}
+func (pump *pump) addCar() {}
+func CreatePump(fuel int, maxCarsInLine int, minTime int, maxTime int) *pump {
+	p := new(pump)
+	p.fuel = fuel
+	p.maxCarsInLine = maxCarsInLine
+	p.line = make([]*car, 0, maxCarsInLine)
+	p.maxTime = maxTime
+	p.minTime = minTime
+	return p
 }
 
 const (
@@ -45,12 +81,9 @@ const (
 func main() {
 
 	var factory carFactory
-	var gaspump pump
-	gaspump.fuel = gas
+	var gaspump = CreatePump(gas, 10, 50, 70)
 	var testcar = factory.getCar()
-	if gaspump.canGetInLine(&testcar) {
-		gaspump.line.PushBack(&testcar)
-	}
+	testcar.fuel = gas
 
 	fmt.Println("Hello, World!")
 
